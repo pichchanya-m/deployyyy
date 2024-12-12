5b9b71fd0c26aac7ea99f304378b1a79509aebcb
pichchanya-m <aimmy007@gmail.com>
2024-12-13 01:13:24 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
 export default defineConfig({
-  root: path.resolve(__dirname, 'frontend'),  // Set the root to the frontend folder
+  root: path.resolve(__dirname, 'frontend'), 
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, 'dist'),  // Output directory outside of frontend
-    emptyOutDir: true,  // Clean up old build files
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
   },
 });
